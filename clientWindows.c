40c4fa8ddaee32558cabe6b00308faa9d41ed0e3
kranquet <ranquetk@gmail.com>
2015-03-25 11:28:45 +0100

    premier commit

---
+++ clientWindows.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+
+int main(){
+
+
+    int SocketId;
+    struct hostent *ServerInfoPt;
+    struct sockaddr_in SockAdrDef;
+    int Data, Result;
+    int OpRes;
+    int ServerPort = 1337;
+    char * ServerName = "19.168.30.56";
+    SocketId = socket(AF_INET, SOCK_STREAM, 0); // Socket creation
+    SockAdrDef.sin_family = AF_INET; // Socket connection
+    SockAdrDef.sin_port = htons(ServerPort);
+    ServerInfoPt = gethostbyname(ServerName);
+    memcpy(&SockAdrDef.sin_addr.s_addr, ServerInfoPt->h_addr, ServerInfoPt->h_length);
+    OpRes = connect(SocketId, (struct sockaddr *) &SockAdrDef, sizeof(struct sockaddr_in));
+    if (OpRes == -1) {
+        fprintf(stderr,"Socket connexion has failed!\n");
+        exit(1);
+    }
+    Data = (int) (rand() % 10000); // Dialog with server
+    send(SocketId,(char *) &Data, sizeof(int), 0);
+    recv(SocketId,(char *) &Result, sizeof(int), 0);
+    close(SocketId);
+    return 0;
+}
